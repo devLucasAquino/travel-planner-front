3fd67493b6d54d387870647d9af5d6713786b31c
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-11 18:08:14 -0300

    feat: adicionado funcao de fechar entrada de convidados

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ export function App() {
     setIsGuestInputOpen(true);
   }
 
+  function closeGuestInput() {
+    setIsGuestInputOpen(false);
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
@@ -23,17 +27,17 @@ export function App() {
           <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
             <div className='flex items-center gap-2 flex-1'>
               <MapPin className='size-5 text-zinc-400'/>
-              <input type="text" placeholder="Para onde você vai?"  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+              <input disabled={isGuestInputOpen} type="text" placeholder="Para onde você vai?"  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
             </div>
             <div className='flex items-center gap-2'>
               <Calendar className='size-5 text-zinc-400'/>
-              <input type="text" placeholder="Quando?"  className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+              <input disabled={isGuestInputOpen} type="text" placeholder="Quando?"  className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
             </div>
 
             <div className='w-px h-6 bg-zinc-800'/> 
             
             {isGuestInputOpen ? (
-              <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+              <button onClick={closeGuestInput} className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
                 Alterar local/data
                 <Settings2 className='size-5'/>
               </button>
