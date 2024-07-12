82886c10088f2ba84a6cdb1285571c8585dd84e9
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-12 17:23:30 -0300

    feat: conhecendo ComponentProps react

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,12 @@
-import { ReactNode } from "react"
+import { ComponentProps, ReactNode } from "react"
 
-interface ButtonProps{
+interface ButtonProps extends ComponentProps<'button'>{
     children: ReactNode;
 }
 
-export function Button({children}: ButtonProps){
+export function Button({children, ...props}: ButtonProps){
     return(
-        <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+        <button {...props} className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
             {children}
         </button>
     )
