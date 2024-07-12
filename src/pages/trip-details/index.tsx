038f06aec74930ff7d3e2402a45a004eafc6055c
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-12 16:23:18 -0300

    finnaly TripDetailsPage structure

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
 
 export function TripDetailsPage(){
     return(
@@ -79,9 +79,68 @@ export function TripDetailsPage(){
                     <div className="space-y-6">
                         <h2 className="font-semibold text-xl">Links importantes</h2>
 
+                        <div className="space-y-5">
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                                    <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                                    https://www.airbnb.com.br/rooms/1047000112142124121241241
+                                    </a>
+                                </div>
+                                <Link2 className="text-zinc-400 size-5 shrink-0"/>
+                            </div>
+
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Regras da casa</span>
+                                    <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                                        https://www.notion.com/pages/1047000112354648336?adults=13&children=0&infants=0&pets=0&wishlist_item_id=11003621872995&check_in=2024-08-17&check_out=2024-08-26&source_impression_id=p3_1717600906_P3DL0E-bJZzguEci&previous_page_section_name=1000                                    
+                                    </a>
+                                </div>
+                                <Link2 className="text-zinc-400 size-5 shrink-0"/>
+                            </div>
+                        </div>
+
+                        <button className='w-full justify-center bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+                            <Plus className='size-5'/>
+                            Cadastrar novo link
+                        </button>
                     </div>
+
                     <div className='w-full h-px bg-zinc-800'/>
-                    <div></div>
+
+                    <div>
+                    <div className="space-y-6">
+                        <h2 className="font-semibold text-xl">Convidados</h2>
+
+                        <div className="space-y-5">
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Jessica White</span>
+                                    <span className="block text-sm text-zinc-400 truncate">
+                                        jessica.white@yahoo.com
+                                    </span>
+                                </div>
+                                <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
+                            </div>
+
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block font-medium text-zinc-100">Dr. Drauzio Spinozza</span>
+                                    <span className="block text-sm text-zinc-400 truncate">
+                                        drauzio@medicamentos.com
+                                    </span>
+                                </div>
+                                <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
+                            </div>
+                        </div>
+
+                        <button className='w-full justify-center bg-zinc-800 text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+                            <UserCog className='size-5'/>
+                            Gerenciar convidados
+                        </button>
+                    </div>
+                    </div>
                 </div>
 
 
