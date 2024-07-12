17f3ec0c19ee101d2fc33ed8ad10465a07ae4f4d
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-12 17:10:52 -0300

    splitting just more index trip-details

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
+import { Plus } from "lucide-react";
 
 import { CreateActivityModal } from "./create-activity-modal";
 import { ImportantLinks } from "./important-links";
 import { Guests } from "./guests";
 import { Activities } from "./activities";
+import { DestinationAndDateHeader } from "./destination-and-date-header";
 
 export function TripDetailsPage(){
 
@@ -20,29 +21,8 @@ export function TripDetailsPage(){
     
     return(
         <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-            <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <MapPin className="size-5 text-zinc-400"/>
-                    <span className="text-lg text-zinc-100">Florianopólis, Brasil</span>
-                </div>
-
-                <div className="flex items-center gap-5">
-                    <div className="flex items-center gap-2">
-                        <Calendar className="size-5 text-zinc-400"/>
-                        <span className="text-zinc-100">17 a 23 de Agosto</span>
-                    </div>
-                    <div className='w-px h-6 bg-zinc-800'/>
-
-                    <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
-                        Alterar local/data
-                        <Settings2 className='size-5'/>
-                    </button>
-                </div>
-
-
-
-
-            </div>
+            
+            <DestinationAndDateHeader />
 
             <main className="flex gap-16 px-4">
 
