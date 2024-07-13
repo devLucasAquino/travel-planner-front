70b3285d2f9379a86c24e7aa65981b1cf2ac9bab
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-07-12 21:51:36 -0300

    feat: registrando uma nova atividade

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -31,7 +31,7 @@ export function Guests(){
                         return(
                             <div key={participant.id} className="flex items-center justify-between gap-4">
                                 <div className="space-y-1.5">
-                                    <span className="block font-medium text-zinc-100">{participant.name ?? ´Convidado ${index}´}</span>
+                                    <span className="block font-medium text-zinc-100">{participant.name ?? `Convidado ${index}`}</span>
                                     <span className="block text-sm text-zinc-400 truncate">
                                         {participant.email}
                                     </span>
